eed04928e4f30c27c88c504ca6693f8f37ce7cc6
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-09 14:57:51 +0300

    filter and test

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import App from './App';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
+import {addCharacterById} from './actions';
 
 const store = createStore(rootReducer);
+store.subscribe(()=> console.log('store', store.getState()));
+store.dispatch(addCharacterById(3));
 
 console.log(store.getState());
 ReactDOM.render(
