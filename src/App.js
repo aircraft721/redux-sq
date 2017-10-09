78d9f74ce7e668477a6c6a8cfeb45d394311a3dc
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-09 16:32:10 +0300

    map state to props and return character names

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterList from './components/CharacterList';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				sss
+				<CharacterList />
 			</div>
 		);
 	}
