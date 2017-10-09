13b7003036b8e15810c62e973757b8e67be0c772
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-10-09 14:38:49 +0300

    set up the store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import characters_json from '../data/characters.json';
 
-export const characters = (state=characters_json, action) => {
+const characters = (state=characters_json, action) => {
     switch(action.type){
         default: 
             return state;
     }
 }
+
+export default characters;
